07668f30167b1be6720deb6e211f56e26b437934
Bruno Ruf <brunoruf.cml@gmail.com>
2021-03-26 18:17:03 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+$(window).ready(function(){
+    let $textoTarefa = $("#texto-tarefa"); 
+    $("#adicionar-tarefa").click(function(){
+         
+        if ($textoTarefa.val() == 0){
+            alert("Você não pode adicionar uma tarefa vazia!");
+        }else{
+            let novaTarefa = {nomeTarefa: $textoTarefa, id: gerarId(), idEdicao: gerarIdEdicao()};
+            adicionarTarefa(novaTarefa);
+            $("#texto-tarefa").val("");           
+        }     
+    });
+
+    $("#texto-tarefa").keypress(function(e){ 
+        if (e.which == 13 && $textoTarefa.val() == 0){
+            alert("Você não pode adicionar uma tarefa vazia!");
+        }else if (e.which == 13 && $textoTarefa.val() != 0){
+            let novaTarefa = {nomeTarefa: $textoTarefa, id: gerarId(), idEdicao: gerarIdEdicao()};
+            adicionarTarefa(novaTarefa);
+            $("#texto-tarefa").val("");
+        }
+    });
+
+function gerarId(){
+    return Number(Math.floor(Math.random() * 3000));
+}
+function gerarIdEdicao(){
+    return Number(Math.floor(Math.random() * -3000));
+}
+
+function adicionarTarefa(novaTarefa){
+    let botaoEditar = `<button class = 'botao-acao' onclick = 'editarTarefa(${novaTarefa.idEdicao})'>Editar</button>`;
+    let botaoExcluir = `<button class = 'botao-acao' onclick = 'excluirTarefa(${novaTarefa.id})'>Excluir</button>`;
+    $("#mostrar").append(`<li id = '${novaTarefa.id}'></li>`);
+    $(`li#${novaTarefa.id}`).append(`<span id = '${novaTarefa.idEdicao}'>${novaTarefa.nomeTarefa.val()}</span>`);   
+    $(`li#${novaTarefa.id}`).append(`<div>${botaoEditar + botaoExcluir}</div>`);
+}
+
+});
+
+function editarTarefa(idEdicao){
+    $(`#${idEdicao}`).replaceWith(`<div><input id = 'texto-edicao' placeholder = 'Atualizar Tarefa'></input></div>`);
+    $("#texto-edicao").keypress(function(e){
+        let textoEdicao = $("#texto-edicao");
+        if (e.which == 13 && textoEdicao.val() == 0){
+            alert("Você não pode salvar uma tarefa vazia!");
+        }else if (e.which == 13 && textoEdicao.val() != 0){
+            $("#texto-edicao").replaceWith(`<span id = '${idEdicao}'>${textoEdicao.val()}</span>`)      
+        }
+    });
+}
+
+function excluirTarefa(idTarefa){
+    let exclusao = window.confirm("Deseja realmente remover sua tarefa?");
+    if (exclusao){
+        $(`li#${idTarefa}`).remove();
+    }    
+}
